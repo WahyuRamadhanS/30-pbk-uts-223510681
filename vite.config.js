a518edc68e0dc42b644d66075ca024c4678c98f4
Your Name <wahyuramadhan.s31@student.uir.ac.id>
2024-05-13 18:20:33 +0700

    tugas10

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      external: ['/C:/Users/LENOVO/Downloads/Untitled design (4).png']
+    }
+  }
+});
